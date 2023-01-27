b8c0b0f28b6914e4053b9bc07a55e4f836bd71cb
Cameron Khare <camkhare@Camerons-MacBook-Air.local>
2023-01-27 10:20:28 -0600

    initial commit

---
+++ python_review.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+
+# this is a single line comment
+
+"""
+Task:
+- ask the user for one number
+- ask the user for another number
+- print the sum of their numbers
+"""
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+
+total = number_1 + number_2
+print(total)
+
+if total > 10:
+	print("booooo")
+elif total <= 8:
+	print("Yayayya")
+else: #must be 10
+	print("Your number is 10")
+
+for i in range(total):
+	print(i)
